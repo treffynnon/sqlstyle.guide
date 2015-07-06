974faf243d9770b21966e8c63aea5f88c2db8afe
Simon Holywell <simon@emosaic.co.uk>
2015-07-06 16:53:43 +0100

    Styling and scroll to implemented

--- scripts.js
+++ scripts.js
@@ -7,3 +7,25 @@ hljs.initHighlightingOnLoad();
 
 {% include anchor.min.js %}
 anchors.add('h2,h3,h4,h5,h6');
+
+/*
+ * ScrollTo code
+ */
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var dest = 0;
+        if(target) {
+            var $target = $(target);
+            dest = $target.offset().top;
+        }
+
+        $('html, body').stop().animate({
+            'scrollTop': dest
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
